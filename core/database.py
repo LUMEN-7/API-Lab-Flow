3f8f0bba7610af51e29980bc80ac225e8a3d3293
Vitorr-AF <vitoraugustofranca7@gmail.com>
2025-10-31 13:47:05 -0300

    Código adaptado para deploy

--- core/database.py
+++ core/database.py
@@ -1,9 +1,8 @@
 import psycopg2
-from segredo import DATABASE_URL
 import os
 
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
