b7c8a7f90e9366ba2d4706c87608a7b1b9cc18ee
Vitorr-AF <vitoraugustofranca7@gmail.com>
2025-10-31 14:54:59 -0300

    Erro corrigido

--- core/auth.py
+++ core/auth.py
@@ -5,6 +5,7 @@ from core.database import get_connection
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.environ.get("SECRET_KEY", "chave_temporaria")
 
 def gerar_token(email_user, cpf_user, admin_user):
     payload = {
